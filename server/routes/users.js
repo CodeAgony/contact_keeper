84ba82f01826ad8364371859ee4d0dc0b48ab991
CodeAgony <yaroslav.bkh@gmail.com>
2019-07-19 19:48:59 +0300

    Add validated User schema

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 // @route    POST api/users
 // @desc     REgister a user
 // @access   Public
-router.post('/', (req, res) => {res.send('Register user');});
+router.post(
+	'/',
+	[
+		check('name', 'Please add your name')
+			.not()
+			.isEmpty(),
+		check('email', 'Please include your valid email').isEmail(),
+		check(
+			'password',
+			'Please enter a password with 6 or more characters'
+		).isLength({
+			min: 6
+		})
+	],
+	(req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		res.send('validation passed');
+	}
+);
 
 module.exports = router;
