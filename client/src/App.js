cac38e93bf0da03e072762066405229c711deaaf
CodeAgony <yaroslav.bkh@gmail.com>
2019-07-26 16:24:57 +0300

    Add routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 
-function App() {
-  return (
-    <div className="App">
-      <p>Cleanup</p>
-    </div>
-  );
-}
+const App = () => (
+	<Router>
+		<Fragment>
+			<Navbar icon="fas fa-id-card" title=" ContactKeeper" />
+			<div className="containter">
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/about" component={About} />
+				</Switch>
+			</div>
+		</Fragment>
+	</Router>
+);
 
 export default App;
