1682a2facd9470355e43745e3971bead891263f4
CodeAgony <yaroslav.bkh@gmail.com>
2019-07-30 10:14:37 +0300

    Set up auth context

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,26 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
 
 const App = () => (
-	<ContactState>
-		<Router>
-			<Fragment>
-				<Navbar icon="fas fa-id-card" title=" ContactKeeper" />
-				<div className="containter">
-					<Switch>
-						<Route exact path="/" component={Home} />
-						<Route exact path="/about" component={About} />
-					</Switch>
-				</div>
-			</Fragment>
-		</Router>
-	</ContactState>
+	<AuthState>
+		<ContactState>
+			<Router>
+				<Fragment>
+					<Navbar icon="fas fa-id-card" title=" ContactKeeper" />
+					<div className="containter">
+						<Switch>
+							<Route exact path="/" component={Home} />
+							<Route exact path="/about" component={About} />
+						</Switch>
+					</div>
+				</Fragment>
+			</Router>
+		</ContactState>
+	</AuthState>
 );
 
 export default App;
