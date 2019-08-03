75aa5208b0a8db60094e6f4b9630f1670385ff23
CodeAgony <yaroslav.bkh@gmail.com>
2019-08-03 17:09:46 +0300

    Build

---
+++ client/build/precache-manifest.e43eb21a9a448f819cbd63e1b1bc1f33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "076cd4f19a81b5ffe3006213bdf381a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f206281cadfaa3fe64a5",
+    "url": "/static/css/main.09639d18.chunk.css"
+  },
+  {
+    "revision": "50d1189a0e4ce9dc0161",
+    "url": "/static/js/2.28dbfef3.chunk.js"
+  },
+  {
+    "revision": "f206281cadfaa3fe64a5",
+    "url": "/static/js/main.29150bfd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
